b1e0d52a313b3fb13d98856c621b9e7dc19931e8
Takayuki Okazaki <watermint@users.noreply.github.com>
2024-05-15 00:04:57 +0900

    add suffix for remote version cache

--- domain/sb_deploy/bin_src_dbx_dst_local.go
+++ domain/sb_deploy/bin_src_dbx_dst_local.go
@@ -1,6 +1,8 @@
 package sb_deploy
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"github.com/watermint/toolbox/domain/dropbox/api/dbx_client"
 	"github.com/watermint/toolbox/domain/dropbox/model/mo_file"
@@ -19,7 +21,7 @@ import (
 
 const (
 	BinSrcDropboxDstLocalVersionCacheLifecycle = 86400
-	BinSrcDropboxDstLocalVersionCacheName      = "sb_deploy-bin_src_dbx_dst_local_version_cache.json"
+	BinSrcDropboxDstLocalVersionCacheName      = "sb_deploy-bin_src_dbx_dst_local_version_cache"
 )
 
 // BinSrcDropboxDstLocalRecipe Deploy binary from Dropbox to local
@@ -175,9 +177,19 @@ func (z binSrcDropboxDstLocalWorkerImpl) GetLocalLatest() (binaryPath string, ve
 	return localVersionPaths[localVersionLatest.String()], localVersionLatest, nil
 }
 
+func (z binSrcDropboxDstLocalWorkerImpl) remoteVersionCacheName() string {
+	seeds := make([]string, 0)
+	seeds = append(seeds, z.recipe.SourceUrl)
+	seeds = append(seeds, z.recipe.Prefix)
+	seeds = append(seeds, z.recipe.Suffix)
+	seeds = append(seeds, z.recipe.BinaryName)
+	seed := sha256.Sum256([]byte(strings.Join(seeds, "-")))
+	return BinSrcDropboxDstLocalVersionCacheName + hex.EncodeToString(seed[:])[0:16] + ".json"
+}
+
 func (z binSrcDropboxDstLocalWorkerImpl) loadRemoteVersionsCache() (versions []es_version.Version, versionPaths map[string]string, found bool) {
 	l := z.ctl.Log()
-	cachePath := filepath.Join(z.ctl.Workspace().Cache(), BinSrcDropboxDstLocalVersionCacheName)
+	cachePath := filepath.Join(z.ctl.Workspace().Cache(), z.remoteVersionCacheName())
 	cacheData, err := os.ReadFile(cachePath)
 	if err != nil {
 		l.Debug("Unable to read cache", esl.Error(err))
@@ -202,7 +214,7 @@ func (z binSrcDropboxDstLocalWorkerImpl) saveRemoteVersionCache(versions []es_ve
 		return err
 	}
 
-	cachePath := filepath.Join(z.ctl.Workspace().Cache(), BinSrcDropboxDstLocalVersionCacheName)
+	cachePath := filepath.Join(z.ctl.Workspace().Cache(), z.remoteVersionCacheName())
 	cache := &BinSrcDropboxDstLocalRemoteVersionCache{
 		CacheTime:    time.Now().Unix(),
 		Versions:     versions,
